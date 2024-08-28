3d727206ace5ebde222177f43daee12512da553b
Andengineer <lamboite10avellaneda@gmail.com>
2024-08-28 08:31:04 -0500

    Hice unas agregaciones en las clases para que funcione correctamente el API de GET y POST

--- StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/entities/Usuario.java
+++ StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/entities/Usuario.java
@@ -29,4 +29,82 @@ public class Usuario {
 
     @Column(name="institucion_educativa",nullable = false, length = 50)
     private String institucion_educativa;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_usuario, String nombres, String apellidos, String dni, String email, String contrasena, int puntos_usuario, String institucion_educativa) {
+        this.id_usuario = id_usuario;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.puntos_usuario = puntos_usuario;
+        this.institucion_educativa = institucion_educativa;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getPuntos_usuario() {
+        return puntos_usuario;
+    }
+
+    public void setPuntos_usuario(int puntos_usuario) {
+        this.puntos_usuario = puntos_usuario;
+    }
+
+    public String getInstitucion_educativa() {
+        return institucion_educativa;
+    }
+
+    public void setInstitucion_educativa(String institucion_educativa) {
+        this.institucion_educativa = institucion_educativa;
+    }
 }
