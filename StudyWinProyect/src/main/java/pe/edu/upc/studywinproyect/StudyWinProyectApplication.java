798d36b9e7e1703c15dd6ca39a447cfcf1d02768
Andengineer <lamboite10avellaneda@gmail.com>
2024-08-27 21:52:02 -0500

    Agregue el proyecto

---
+++ StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/StudyWinProyectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.studywinproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWinProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWinProyectApplication.class, args);
+    }
+
+}
---
+++ StudyWinProyect/src/test/java/pe/edu/upc/studywinproyect/StudyWinProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.studywinproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWinProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
