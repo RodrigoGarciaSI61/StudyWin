f5d2ad2d3d59dad0358c9744c351d0a5fd58e683
NorelVS <norel5678@gmail.com>
2024-09-03 05:19:54 -0500

    agregue el archivo dtos de tipo_recompensa

---
+++ StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/dtos/TipoRecompensaDTO.java
@@ -0,0 +1,23 @@
+package pe.edu.upc.studywinproyect.dtos;
+
+public class TipoRecompensaDTO {
+
+    private int idTipoRecompensa;
+    private String Tipo;
+
+    public int getIdTipoRecompensa() {
+        return idTipoRecompensa;
+    }
+
+    public void setIdTipoRecompensa(int idTipoRecompensa) {
+        this.idTipoRecompensa = idTipoRecompensa;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.Tipo = tipo;
+    }
+}
