95132104285b6d949b301a7f2041e575525b5832
NorelVS <norel5678@gmail.com>
2024-11-12 01:34:10 -0500

    ruc de 10 a 11

--- StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/entities/Asociado.java
+++ StudyWinProyect/src/main/java/pe/edu/upc/studywinproyect/entities/Asociado.java
@@ -20,7 +20,7 @@ public class Asociado implements Serializable {
     private LocalDate fecha_inicio;
     @Column(name="fecha_fin",nullable = true)
     private LocalDate fecha_fin;
-    @Column(name = "ruc", nullable = false, length = 10)
+    @Column(name = "ruc", nullable = false, length = 11)
     private String RUC;
     @Column(name = "imagen",nullable = false)
     private String imagen;
